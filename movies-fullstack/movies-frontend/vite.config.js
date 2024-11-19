55a30e4a5f99345386e341eb5bdfe09d778bf662
serhat-bca <sersen@bergen.org>
2024-11-19 15:46:08 -0500

    fixed proxy.

--- movies-fullstack/movies-frontend/vite.config.js
+++ movies-fullstack/movies-frontend/vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3001",
-      changeOrigin: true,
+      "/api": { target: "http://localhost:3001", changeOrigin: true },
     },
   },
 });
